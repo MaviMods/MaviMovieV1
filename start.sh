6176b9e33aa55d1b3031cc231112665e27579a9f
MaviMods <139610566+MaviMods@users.noreply.github.com>
2023-08-23 15:42:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/MaviMods/LazyPrincess/blob/artificial_inteligence_v7.5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
