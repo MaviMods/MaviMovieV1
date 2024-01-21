ce551742c0bee54afea3a5704e64ca035459cae1
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-01-21 15:13:59 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -276,7 +276,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f'HEY 👋\n\n📌TITLE {search}\n📣YOUR FILE IS READY NOW'
+        cap = f'HEY requested👋\n\n📌TITLE {search}\n📣YOUR FILE IS READY NOW'
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
